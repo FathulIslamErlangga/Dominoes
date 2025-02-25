2957253cab0849510c06f3c949ed706d3451826c
fathulislamerlangga <fathulislamerlangga@gmail.com>
2025-02-25 17:56:38 +0700

    domioes

---
+++ src/components/Dominoes.tsx
@@ -0,0 +1,94 @@
+"use client";
+import dominoesHooks from "@/hooks/dominoes.hooks";
+import React from "react";
+
+const Dominoes = () => {
+  const {
+    filterCard,
+    cards,
+    error,
+    input,
+    sortCardDomino,
+    handleRemove,
+    inputValueNumber,
+    flipDomino,
+    resetValueDomino,
+    handleRemoveDuplicate,
+  } = dominoesHooks();
+  return (
+    <div className="mx-auto  flex flex-col w-[50%] gap-6 mt-10">
+      <div className="bg-gray-100 rounded-md shadow-md  p-5">
+        <h1 className="font-semibold text-lg font-mono">Source</h1>
+        <span>{cards.map((card) => `[${card[0]},${card[1]}] `)}</span>
+      </div>
+      <div className="bg-gray-100 rounded-md shadow-md p-5">
+        <h1 className="font-semibold text-lg font-mono">Double Number</h1>
+        <span>{filterCard.map((card) => `[${card[0]},${card[1]}]`)}</span>
+      </div>
+      <div className="flex gap-2">
+        {cards.map(([a, b], i) => (
+          <div
+            key={i}
+            className="border-2 border-gray-600 p-[5px] max-w-6 text-sm"
+          >
+            <h2>{a}</h2>
+            <div className="border-2 border-gray-600 my-2" />
+            <h2>{b}</h2>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex gap-3">
+        <button
+          className="bg-blue-500 rounded-md border-none p-2 max-w-32 text-white"
+          onClick={() => sortCardDomino("asc")}
+        >
+          Sort (ASC)
+        </button>
+        <button
+          className="bg-blue-500 rounded-md border-none p-2 max-w-32 text-white"
+          onClick={() => sortCardDomino("desc")}
+        >
+          Sort (DESC)
+        </button>
+        <button
+          className="bg-blue-500 rounded-md border-none p-2 max-w-32 text-white flip"
+          onClick={flipDomino}
+        >
+          Flip
+        </button>
+        <button
+          className="bg-blue-500 rounded-md border-none p-2 max-w-32 text-white"
+          onClick={handleRemoveDuplicate}
+        >
+          Remove Dup
+        </button>
+        <button
+          className="bg-blue-500 rounded-md border-none p-2 max-w-32 text-white"
+          onClick={resetValueDomino}
+        >
+          Reset
+        </button>
+      </div>
+      <div className="flex flex-col">
+        <h1 className="text-sm text-red-500 font-mono">{error}</h1>
+
+        <input
+          type="text"
+          placeholder="Input Number"
+          className="p-2 rounded-md border-2 mr-5"
+          value={input}
+          onChange={inputValueNumber}
+        />
+      </div>
+      <button
+        className="bg-blue-500 rounded-md border-none p-2 max-w-32 text-white"
+        onClick={handleRemove}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default Dominoes;
---
+++ src/hooks/dominoes.hooks.tsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useState } from "react";
+
+const dominoesHooks = () => {
+  const initialCard = [
+    [1, 2],
+    [1, 1],
+    [4, 1],
+    [3, 3],
+    [6, 1],
+    [5, 1],
+    [3, 2],
+    [2, 3],
+    [3, 1],
+    [5, 1],
+  ];
+  const [cards, setCards] = useState(initialCard);
+  const [input, setInput] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const filterCard = cards.filter((card) => card[0] === card[1]);
+
+  const sortCardDomino = (sort: "asc" | "desc") => {
+    const sortValue = [...cards].sort((a, b) => {
+      const sortA = a[0] + a[1];
+      const sortB = b[0] + b[1];
+      return sort === "asc" ? sortA - sortB : sortB - sortA;
+    });
+    setCards(sortValue);
+  };
+
+  const removeNumberDomino = (total: number) => {
+    const number = cards.filter(([a, b]) => a + b !== total);
+    setCards(number);
+  };
+
+  const flipDomino = () => {
+    setCards(cards.map(([a, b]) => [b, a]));
+  };
+  const resetValueDomino = () => {
+    setCards(initialCard);
+  };
+
+  const inputValueNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    const numberValue = Number(value);
+
+    if (isNaN(numberValue)) {
+      setError("Inputan hanya boleh angka");
+      console.log("Error:", error);
+
+      setTimeout(() => {
+        setError("Inputan hanya boleh angka");
+      }, 2000);
+    } else {
+      setError("");
+      setInput(value);
+    }
+  };
+
+  const handleRemove = () => {
+    const total = Number(input);
+    removeNumberDomino(total);
+    setInput("");
+  };
+  const handleRemoveDuplicate = () => {
+    const uniqueCards = cards.reduce((acc: number[][], [a, b]: number[]) => {
+      const exists = acc.some(
+        ([x, y]) => (x === a && y === b) || (x === b && y === a)
+      );
+      return exists ? acc : [...acc, [a, b]];
+    }, []);
+    setCards(uniqueCards);
+  };
+  return {
+    cards,
+    filterCard,
+    input,
+    error,
+    sortCardDomino,
+    handleRemove,
+    inputValueNumber,
+    flipDomino,
+    resetValueDomino,
+    handleRemoveDuplicate,
+  };
+};
+
+export default dominoesHooks;
